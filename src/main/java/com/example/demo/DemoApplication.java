10c148a544051ebba109c5258ac7f271df48fc0c
aman <azewgee@gmail.com>
2020-05-13 22:53:29 -0400

    hew commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,19 +3,22 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	@RequestMapping("/")
-	@ResponseBody
+//	@ResponseBody
 	String home() {
 		return "Hello World!";
 	}
 
+	@GetMapping("/h/{name}")
+	public String getName(@PathVariable String name) {
+		return "Hello " + name ;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
